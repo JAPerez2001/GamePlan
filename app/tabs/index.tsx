b72b2abb9bbe0afc028888b88cd5c023888d279a
ryliek456 <dorrough.rylie@yahoo.com>
2024-10-21 20:07:15 -0500

    updates

---
+++ app/tabs/index.tsx
@@ -0,0 +1,34 @@
+import { Text, View } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function ChatScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}} >
+      <Text>Chat will go here</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function tabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name = "Chat" component={ChatScreen} />
+    </Tab.Navigator>
+  )
+}
+
+export default function Chat() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Changed</Text>
+    </View>
+  );
+}
